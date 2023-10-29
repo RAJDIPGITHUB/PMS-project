36dd17becf2bebb1797ff64b618c3ac70a882823
Rajdip Mondal <rajdipmondal27092002@gmail.com>
2023-10-29 12:48:52 +0530

    faculty files

--- report.php
+++ report.php
@@ -19,7 +19,7 @@ if($role=="Admin")
 <style>
 	body
 	{
-		background-image:url(../background.png);
+		background-image:url(background.png);
 		background-repeat: no-repeat; 
 		background-attachment: fixed;
 		background-size: 100% 100%;
@@ -44,64 +44,120 @@ if($role=="Admin")
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr bgcolor="#99CCFF">
       <th width="5%" scope="col"><h4>&nbsp;</h4></th>
-      <th width="12%" scope="col"><a href="student.php">Add Student</a></th>
-      <th width="11%" scope="col"><a href="faculty.php">Add Faculty</a></th>
-      <th width="11%" scope="col"><a href="stsearch.php">Search Student</a></th>
-      <th width="11%" scope="col"><a href="fa_search.php">Search Faculty </a></th>
-      <th width="11%" scope="col"><a href="allocate.php">Allocate</a></th>
-      <th width="11%" scope="col"><a href="skill.php">Skill Matrix</a></th>
-      <th width="11%" scope="col"><a href="report.php">Reports</a></th>
-      <th width="11%" scope="col"><a href="../logout.php">Logout</a></th>
+      <th width="12%" scope="col"><a href="ADMIN/student.php">Add Student</a></th>
+      <th width="11%" scope="col"><a href="ADMIN/faculty.php">Add Faculty</a></th>
+      <th width="11%" scope="col"><a href="ADMIN/stsearch.php">Search Student</a></th>
+      <th width="11%" scope="col"><a href="ADMIN/fa_search.php">Search Faculty </a></th>
+      <th width="11%" scope="col"><a href="ADMIN/allocate.php">Allocate</a></th>
+      <th width="11%" scope="col"><a href="ADMIN/skill.php">Skill Matrix</a></th>
+      <th width="11%" scope="col"><a href="#">Reports</a></th>
+      <th width="11%" scope="col"><a href="logout.php">Logout</a></th>
     <th width="6%" scope="col">&nbsp;</th>
   </tr>
-</table>
-    <br/><br/><br/>
-    <table  border="1" align="center" bgcolor="red">
-            <?php
-                echo "<tr>";
-                echo "<th>"."Meeting ID"."</th>";
-                echo "<th>" ?> &nbsp; <?php "</th>";
-                echo "<th>"."Faculty ID"."</th>";
-                echo "<th>" ?> &nbsp; <?php "</th>";
-                echo "<th>"."Student ID"."</th>";
-                echo "<th>" ?> &nbsp; <?php "</th>";
-                echo "<th>"."Date"."</th>";
-                echo "<th>" ?> &nbsp; <?php "</th>";
-                echo "<th>"."Time"."</th>";
-                echo "<th>" ?> &nbsp; <?php "</th>";
-                echo "<th>"."Description"."</th>";
-                echo "</tr>";
-                include './connection.php';
-                        $sql1="select * from meeting ";
-                        $rec=mysqli_query($conn, $sql1);
-                        while ($std=mysqli_fetch_assoc($rec))
-                        {
-                           ?> <tr bgcolor="beige" align="center"><?php
-                            echo "<td>".$std['meeting_id']."<td/>";
-                            echo "<td>".$std['f_id']."<td/>"; 
-                            echo "<td>".$std['s_id']."<td/>"; 
-                            echo "<td>".$std['date']."<td/>"; 
-                            echo "<td>".$std['time']."<td/>"; 
-                            echo "<td>".$std['desc']."<td/>"; 
-                            ?>  </tr> <?php 
-                        }
-            ?>
-        </table>
-    </form>
  <?php
- 
 }
 elseif($role=="Faculty")    
 {
-    header('Location:../Admin.php'); 
 ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<style>
+	body
+	{
+		background-image:url(background.png);
+		background-repeat: no-repeat; 
+		background-attachment: fixed;
+		background-size: 100% 100%;
+	}
+        a link{
+            text-decoration: none;
+        }
+</style>
+<title>Project Management System</title>
+</head>
+<div>
+<body>
+    <table width="100%"  border="0"cellspacing="00" cellpadding="00">
+  <tr bgcolor="#D2691E">
+    <th width="74" scope="col">&nbsp;</th>
+    <th width="164" scope="col"><img src="images/logo1.png" alt="LOGO"></th>
+    <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
+    <th width="140" scope="col"><font color="White" size="5">
+    <?php
+    print $role;
+    echo "<br/>";
+    print $user;
+    ?>
+        </font></th>
+    <th width="63" scope="col">&nbsp;</th>
+  </tr>
+    </table>
+  <table width="100%" border="0" cellspacing="00" cellpadding="00">
+      <tr bgcolor="#99CCFF">
+      <th width="7%" scope="col"><h4>&nbsp;</h4></th>
+      <th width="15%" scope="col"><a href="FACULTY/skill.php">Skill Matrix</a></th>
+      <th width="14%" scope="col"><a href="FACULTY/view.php">View</a></th>
+      <th width="15%" scope="col"><a href="FACULTY/mail.php">Mail</a></th>
+      <th width="13%" scope="col"><a href="FACULTY/meeting.php">Meeting</a></th>
+      <th width="15%" scope="col"><a href="FACULTY/#">Reprots</a></th>
+    <th width="15%" scope="col"><a href="logout.php">Logout</a></th>
+    <th width="6%" scope="col">&nbsp;</th>
+  </tr>
  <?php
 }
 else   
 {
-    header('Location:../Admin.php'); 
 ?>
-      
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<style>
+	body
+	{
+		background-image:url(background.png);
+		background-repeat: no-repeat; 
+		background-attachment: fixed;
+		background-size: 100% 100%;
+	}
+        a link{
+            text-decoration: none;
+        }
+</style>
+<title>Project Management System</title>
+</head>
+<div>
+<body>
+    <table width="100%"  border="0"cellspacing="00" cellpadding="00">
+  <tr bgcolor="#D2691E">
+    <th width="74" scope="col">&nbsp;</th>
+    <th width="164" scope="col"><img src="images/logo1.png" alt="LOGO"></th>
+    <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
+    <th width="140" scope="col"><font color="White" size="5">
+	<?php
+    print $role;
+    echo "<br/>";
+    print $user;
+    ?></font></th>
+    <th width="63" scope="col">&nbsp;</th>
+  </tr>
+    </table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#99CCFF">
+      <th width="7%" scope="col"><h4>&nbsp;</h4></th>
+    <th width="13%" scope="col"><a href="STUDENT/project.php">Project</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="STUDENT/skill.php">View Skill Matrix</a></th>
+    <th width="11%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="STUDENT/mail.php">Mail</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="logout.php">Logout</a></th>
+    <th width="6%" scope="col">&nbsp;</th>
+  </tr>
 <?php
 }
 ?>
@@ -110,4 +166,9 @@ else
   <?php
 }
 ?>
-   
+    
+    
+</p>
+<p>&nbsp;</p>
+  </table>
+</table></body>
