b39c0e43d153b2d8e3ca0b47cef62582aef2c53c
Rajdip Mondal <rajdipmondal27092002@gmail.com>
2023-10-29 12:49:30 +0530

    student files

--- mail.php
+++ mail.php
@@ -8,14 +8,14 @@ include '../connection.php';
 
 if (isset($_POST['submit']))
 {
-            $to=$_POST['student']; 
+            $to=$_POST['to']; 
            $msg=$_POST['msg'];
            
            
           if (!empty($to))
            {
               
-            $sql= "INSERT INTO `pmas`.`mail` (`mail_id`, `s_id`, `f_id`, `msg`) VALUES ('', '$to', '$user', '$msg');";
+            $sql= "INSERT INTO `pmas`.`st_mail` (`s_mail_id`, `s_id`, `f_id`, `mag`) VALUES ('', '$user', '$to', '$msg');";
                 mysqli_query($conn, $sql);
                 $conn->close();
                 header('Location:mail.php');  
@@ -36,16 +36,21 @@ else
 {
 if($role=="Admin")
 {
-    header("location:../Admin.php?image=image.php");
 ?>
-
-
  <?php
+ header('Location:../Admin.php');
 }
 elseif($role=="Faculty")    
 {
 ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    
+ <?php
+ header('Location:../Admin.php');
+}
+else   
+{
+?>
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <link rel="stylesheet" type="text/css" href="../css.css">
@@ -69,32 +74,44 @@ elseif($role=="Faculty")
     <th width="164" scope="col"><a href="../Admin.php"><img src="../logo1.png" alt="LOGO"/></a></th>
     <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
     <th width="140" scope="col"><font color="White" size="5">
-    <?php
+	<?php
     print $role;
-    echo "<br/>";
+        echo "<br/>";
     print $user;
-    ?>
-        </font></th>
+    ?></font></th>
     <th width="63" scope="col">&nbsp;</th>
   </tr>
     </table>
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr bgcolor="#99CCFF">
-      <th width="7%" scope="col"> <h4>&nbsp;</h4></th>
-    <th width="15%" scope="col"><a href="skill.php">Skill Matrix</a></th>
-    <th width="14%" scope="col"><a href="view.php">View</a></th>
-    <th width="15%" scope="col"><a href="mail.php">Mail</a></th>
-    <th width="13%" scope="col"><a href="meeting.php">Meeting</a></th>
-    <th width="15%" scope="col"><a href="report.php">Reprots</a></th>
-    <th width="15%" scope="col"><a href="../logout.php">Logout</a></th>
+  <table width="100%" border="0" cellspacing="00" cellpadding="00">
+      <tr bgcolor="#99CCFF">
+          <th width="7%" scope="col"><h4>&nbsp;</h4></th>
+          <th width="13%" scope="col"><a href="project.php">Project</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="skill.php">View Skill Matrix</a></th>
+    <th width="11%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="mail.php">Mail</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="../logout.php">Logout</a></th>
     <th width="6%" scope="col">&nbsp;</th>
   </tr>
-  </table>
-    <bt/><br/><br/>
-      <form method="post" action="mail.php">
+<?php
+}
+?>
+</table>
+<p>
+  <?php
+}
+?>
+    
+    
+</p>
+<p>&nbsp;</p>
+
+
+<form method="post" action="mail.php">
     <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center">
         <tr bgcolor="beige">   <th><h4>&nbsp;</h4></th>
-            <th><input  style="width: 5em;  height: 2em; font-size: 15px;" type="submit" value="Compose" name="compose"/></th>
+                <th><input style="width: 5em;  height: 2em; font-size: 15px;" type="submit" value="Compose" name="compose"/></th>
                 <th>&nbsp;</th>
                 <th><input style="width: 5em;  height: 2em; font-size: 15px;" type="submit" value="Inbox" name="inbox"/></th>
                 <th>&nbsp;</th>
@@ -115,26 +132,9 @@ elseif($role=="Faculty")
                         <br/><br/>
                         <div style="background-color: beige; width: 40%; margin-left: 5%; border: black;">
                         <br/><br/>
-                        <font size="5"> To : &nbsp;&nbsp;</font> 
-                        <?php
-                        include '../connection.php';
-                         $sql="select s_id from project where f_id='$user';";
-                         $result=  mysqli_query($conn, $sql)
-                         ?> <select name="student" style="width: 10em; height: 2em; font-size: 15px;">
-                        <option >Choose From Supervisory</option>
-                        <?php
-                        while($row = mysqli_fetch_assoc($result))
-                        {
-                            $category= $row['s_id'];
-                            ?>
-                 <option selected="selected" value="<?php echo $category; ?>"><?php echo $category;?></option>
-                 <?php
-                 }
-     ?>
-            </select> 
-                        <br/><br/>
+                        <font size="5">To : &nbsp;&nbsp; </font><input id="in" type="text" name="to" readonly value="<?php echo $std["f_id"];?>"/><br/><br/>
                         <font size="5">  From : </font><input id="in" type="text" name="from" value="<?php echo $user;?>" readonly/><br/><br/>
-                        <textarea  name="msg" cols="35" rows="5" placeholder="Message" ></textarea><br/><br/>
+                        <textarea name="msg" cols="30" rows="5" placeholder="Message" ></textarea><br/><br/>
                         <input id="bt" type="submit" value="Send" name="submit"/>
                         <br/><br/>
                         </div>
@@ -147,7 +147,7 @@ elseif($role=="Faculty")
                     {
                         ?>  
                         
-                            <table width="40%" cellpadding="05" cellspacing="01" border="0" align="center" bgcolor="red">  
+                            <table width="40%" cellpadding="0" cellspacing="2" border="0" align="center" bgcolor="red">  
 
                     <?php
                                             echo "<br/>";
@@ -158,7 +158,7 @@ elseif($role=="Faculty")
                     echo "<th>" ?> &nbsp; <?php "</th>";
                     echo "<th>"."MESSAGE"."</th>";
                     echo "</tr>";
-                        $sql1="select * from st_mail where f_id ='$user'";
+                        $sql1="select * from mail where s_id ='$user'";
                         $rec=mysqli_query($conn, $sql1);
                         
                         echo "<tr>";
@@ -168,8 +168,8 @@ elseif($role=="Faculty")
                             {
                                ?> <tr bgcolor="beige" align="center"><?php
                             //echo "<tr>";
-                            echo "<td>".$std['s_id']."<td/>";
-                            echo "<td>".$std['mag']."<td/>"; 
+                            echo "<td>".$std['f_id']."<td/>";
+                            echo "<td>".$std['msg']."<td/>"; 
                             ?>  </tr> <?php 
                             //echo "<tr/>";
                             }
@@ -193,15 +193,15 @@ elseif($role=="Faculty")
                     echo "<th>" ?> &nbsp; <?php "</th>";
                     echo "<th>"."MESSAGE"."</th>";
                     echo "</>";
-                        $sql1="select * from mail where f_id ='$user' ";
+                        $sql1="select * from st_mail where s_id ='$user' ";
                         $rec=mysqli_query($conn, $sql1);
                         
                         echo "<tr>";
                         while ($std=mysqli_fetch_assoc($rec))
                         {
                            ?> <tr bgcolor="beige" align="center"><?php
-                            echo "<td>".$std['s_id']."<td/>";
-                            echo "<td>".$std['msg']."<td/>"; 
+                            echo "<td>".$std['f_id']."<td/>";
+                            echo "<td>".$std['mag']."<td/>"; 
                             ?>  </tr> <?php 
                         }
                         //echo "<tr/>";
@@ -213,21 +213,4 @@ elseif($role=="Faculty")
         
     </table>
 </form>
- <?php
-}
-else   
-{
-    header("location:../Admin.php?image=image.php");
-?>
-      
-<?php
-}
-?>
-</table>
-<p>
-  <?php
-}
-?>
-   
-
     
--- skill.php
+++ skill.php
@@ -3,29 +3,18 @@ session_start();
 $user =  $_SESSION['Email'];
 $role = $_SESSION['Role'];
 
-
 include '../connection.php';
-if(isset($_POST['update']))
-{
-           $domain=$_POST['domain'];
-           $research=$_POST['research']; 
-           $others=$_POST['others'];
-           
-           if (!empty($domain)|| !empty($research)||!empty($others))
-           {
-              
-            $sql= "UPDATE `pmas`.`faculty` SET `domain` = '$domain', `research` = '$research', `others` = '$others' WHERE `faculty`.`f_id` = '$user';";
+
+if(isset($_POST['allocate']))
+        {
+           $id=$_POST['facultyid'];  
+            $sql= "INSERT INTO `pmas`.`request` (`request_id`, `s_id`, `f_id`) VALUES ('', '$user', '$id');";
                 mysqli_query($conn, $sql);
                 $conn->close();
                 header('Location:skill.php');  
-           }
-        else
-            
-        {
-              echo 'Please fill up all fields';
-              header('Location:skill.php');
-        }  
-}
+                  
+        }
+
 
 
 
@@ -43,6 +32,7 @@ if($role=="Admin")
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <style>
 	body
 	{
@@ -51,23 +41,31 @@ if($role=="Admin")
 		background-attachment: fixed;
 		background-size: 100% 100%;
 	}
+        a link{
+            text-decoration: none;
+        }
 </style>
 <title>Project Management System</title>
 </head>
 <div>
-    <body>
-<?php
-   header('Location:../Admin.php');
-   ?>
+<body>
+
  <?php
+ header('Location:../Admin.php');
 }
 elseif($role=="Faculty")    
 {
+    header('Location:../Admin.php');
 ?>
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    
+ <?php
+}
+else   
+{
+?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <link rel="stylesheet" type="text/css" href="../css.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <style>
@@ -92,76 +90,124 @@ elseif($role=="Faculty")
     <th width="164" scope="col"><a href="../Admin.php"><img src="../logo1.png" alt="LOGO"/></a></th>
     <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
     <th width="140" scope="col"><font color="White" size="5">
-    <?php
+	<?php
     print $role;
     echo "<br/>";
     print $user;
-    ?>
-        </font></th>
+    ?></font></th>
     <th width="63" scope="col">&nbsp;</th>
   </tr>
     </table>
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr bgcolor="#99CCFF">
-      <th width="7%" scope="col"> <h4>&nbsp;</h4></th>
-    <th width="15%" scope="col"><a href="skill.php">Skill Matrix</a></th>
-    <th width="14%" scope="col"><a href="view.php">View</a></th>
-    <th width="15%" scope="col"><a href="mail.php">Mail</a></th>
-    <th width="13%" scope="col"><a href="meeting.php">Meeting</a></th>
-    <th width="15%" scope="col"><a href="#">Reprots</a></th>
-    <th width="15%" scope="col"><a href="../logout.php">Logout</a></th>
+  <table width="100%" border="0" cellspacing="00" cellpadding="00">
+      <tr bgcolor="#99CCFF">
+          <th width="7%" scope="col"><h4>&nbsp;</h4></th>
+          <th width="13%" scope="col"><a href="project.php">Project</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="skill.php">View Skill Matrix</a></th>
+    <th width="11%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="mail.php">Mail</a></th>
+    <th width="12%" scope="col">&nbsp;</th>
+    <th width="13%" scope="col"><a href="../logout.php">Logout</a></th>
     <th width="6%" scope="col">&nbsp;</th>
   </tr>
-  </table>
-      <form action="skill.php" method="post">
-          <div style="background-color:beige; border:1px solid black; width:40%; margin-left:30%; margin-top:100px;">
-              <br/>
-              <table   cellspacing="05" cellpadding="05" align="center" width="100%">
-    <tr>
-      <td>&nbsp;</td>
-      <td align="right"><font size="5">Domain : </font></td>
-      <td><input id="in" type="text" name="domain"/><font color="red">*</font></td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td align="right"><font size="5">Research : </font></td>
-      <td><input id="in" type="text" name="research"/><font color="red">*</font></td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td align="right"><font size="5">Other Skill (s) if any : </font></td>
-      <td><input id="in" type="text" name="others"/></td>
-      <td>&nbsp;</td>
-    </tr>
-    
-    <tr>
-        <td colspan="4" align="center"><input id="bt" type="submit" name="update" value="Update" /></td>
-    </tr>
-  </table>
-          </div>
-  </form>
- <?php
-}
-else   
-{
-?>
-    <?php
-   header('Location:../Admin.php');
-   ?>
 <?php
 }
 ?>
 </table>
-
-  <p>
-    <?php
+<p>
+  <?php
 }
 ?>
     
     
+    <div style="background-color: #D2691E; width: 50%; margin-left: 25%; margin-top: 0px; ">
+        <br/><br/>
+    <form method="post" action="skill.php">
+        <table align="center" bgcolor="beige" width="30%">
+        <tr>
+            
+            <td align="center">
+              <br/><br/>  
+    <?php
+            include '../connection.php';
+             $sql="select f_id from faculty";
+             $result=  mysqli_query($conn, $sql)
+             ?> <select name="faculty">
+                 <option selected="selected"><h3>Supervisors</h3></option>
+                 <?php
+                 while($row = mysqli_fetch_assoc($result))
+                 {
+                     $category= $row['f_id'];
+                     ?>
+                 <option selected="selected" value="<?php echo $category; ?>"><?php echo $category;?></option>
+                 <?php
+                 }
+     ?>
+            </select>  <br/><br/>
+        <input type="submit" name="asses" value="View Skill Matrix"/><br/><br/>
+            </td>
+            
+        </tr>
+    </table>    
+         </form>
     
-  </p>
-  <p>&nbsp;</p>
-    </body>
+    
+    
+    <br/><br/><br/>
+    
+    
+    
+    
+    <form method="post" action="skill.php">
+       <div style="background-color: beige; width: 60%; margin-left: 20%;"> 
+    <table width="50%" cellpadding="5" cellspacing="5" border="0" align="center">
+    <?php
+            if (isset($_POST['asses']))
+            {   
+                $fid=$_POST['faculty'];          
+                echo "<tr>";?>
+                <td>Faculty ID</td>
+                <td><input type="text" name="facultyid"  readonly value="<?php echo $fid;?>" readonly</td>
+                <?php
+                echo "</tr>";
+                
+                $sql1="select * from faculty where f_id ='$fid' ";
+                $rec=mysqli_query($conn, $sql1);
+                while ($std= mysqli_fetch_assoc($rec))
+                {
+                echo "<tr>";
+                echo "<td>"."Name"."</td>";
+                echo "<td>"?> <input type="text" name="stid" readonly value="<?php echo $std['name'];?>"/> <?php "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td>"."Qualification"."</td>";
+                echo "<td>"?> <input type="text" name="faqu" readonly value="<?php echo $std['qualification'];?>"/> <?php "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td>"."Domain"."</td>";
+                echo "<td>"?> <input type="text" name="fad" readonly value="<?php echo $std['domain'];?>"/> <?php "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td>"."Research"."</td>";
+                echo "<td>"?> <input type="text" name="far" readonly value="<?php echo $std['research'];?>"/> <?php "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td>"."Others"."</td>";
+                echo "<td>"?> <input type="text" name="fao" readonly value="<?php echo $std['others'];?>"/> <?php "</td>";
+                echo "</tr>";
+                echo "<tr>";
+                echo "<td></td>";
+                echo "<td>"?> <input type="submit" name="allocate" readonly value="Request For Allocate"/> <?php "</td>";
+                echo "</tr>";
+                
+                }
+                
+               
+            }
+    ?>
+                
+    </table>
+        </div>
+    </form>
+    <br/><br/><br/>
+    </div>
